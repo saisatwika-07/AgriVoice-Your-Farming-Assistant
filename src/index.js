fb741ae08c28866b581a20c555cea0615d484280
ReymouldRevanth <revanth.danduboina@reymould.com>
2024-01-26 19:36:59 +0530

    Added deployment port for index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 import  userDetails from "./mongodb.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
